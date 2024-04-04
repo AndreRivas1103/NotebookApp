a48c9474b94cf3edace55468292af525a399266a
AndreRivas1103 <andrerivasgarcia85@gmail.com>
2024-04-03 22:38:39 -0500

    add changes

--- notebook/model/notebook.py
+++ notebook/model/notebook.py
@@ -1,1 +1,31 @@
 from datetime import datetime
+from dataclasses import dataclass
+
+@dataclass
+class Note:
+    HIGH: str = "HIGH"
+    MEDIUM: str = "MEDIUM"
+    LOW: str = "LOW"
+
+    def __init__(self, code: int, title: str, text: str, importance: str):
+        pass
+
+    def add_tag(self, tag: str):
+        pass
+
+    def __str__(self) -> str:
+        pass
+    
+class Notebook:
+    def __init__(self):
+        pass
+    
+    def add_note(self, title: str, text: str, importance: str):
+        pass
+    
+    def importan_notes(self) -> list[Note]:
+        pass
+    
+    def tag_notes_count(self) -> dict[str, int]:
+        pass
+    
